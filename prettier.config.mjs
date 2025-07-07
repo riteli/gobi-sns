83221c5c9185e9b7d4363683dbd5891743a19253
Ritsu <r.ogata.0629@gmail.com>
2025-07-07 13:09:39 +0900

    Initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  endOfLine: 'lf',
+};
+
+export default config;
