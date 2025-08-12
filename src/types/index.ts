0b899116a7b0c01cd7ea44d8cc5a495214a4ad5d
Ritsu <r.ogata.0629@gmail.com>
2025-08-12 17:11:18 +0900

    refactor(profile): プロフィールページをモダンなフォーム実装に改修

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 import { type Database } from './database.types';
 
-type Post = Database['public']['Tables']['posts']['Row'];
-type Profile = Database['public']['Tables']['profiles']['Row'];
+export type Post = Database['public']['Tables']['posts']['Row'];
+export type Profile = Database['public']['Tables']['profiles']['Row'];
 
 export type PostWithProfile = Post & {
   profiles: Pick<Profile, 'user_name'> | null;
