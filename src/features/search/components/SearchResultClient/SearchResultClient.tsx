c677c01679c7e154100cf62e0654b8fd9845b723
Ritsu <r.ogata.0629@gmail.com>
2025-09-24 12:58:23 +0900

    fix:デプロイ用にコードを修正

--- src/features/search/components/SearchResultClient/SearchResultClient.tsx
+++ src/features/search/components/SearchResultClient/SearchResultClient.tsx
@@ -5,34 +5,17 @@ import PostList from '@/features/posts/components/PostList/PostList';
 import { type PostWithProfile } from '@/types';
 
 type SearchResultClientProps = {
-  query: string;
-  initialPosts: PostWithProfile[] | null;
+  posts: PostWithProfile[] | null;
   timelineContextValue: TimelineContextType;
 };
 
 /**
  * 検索結果の投稿一覧と、それらが依存するContextを提供するクライアントコンポーネント
  */
-export const SearchResultClient = ({
-  query,
-  initialPosts,
-  timelineContextValue,
-}: SearchResultClientProps) => {
-  const fetcher = useCallback(
-    (page: number, pageSize: number) => {
-      return searchPosts(query, page, pageSize);
-    },
-    [query],
-  );
-
-  const { posts, isLoading, ref } = useInfiniteScroll(initialPosts, fetcher);
-
+export const SearchResultClient = ({ posts, timelineContextValue }: SearchResultClientProps) => {
   return (
     <TimelineContext.Provider value={timelineContextValue}>
       <PostList posts={posts} />
-
-      <div ref={ref} />
-      {isLoading && <p style={{ textAlign: 'center' }}>読み込み中...</p>}
     </TimelineContext.Provider>
   );
 };
